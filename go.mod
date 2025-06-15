1f8a078ae4ff79245e5fae4b05ec27797506fddd
NathanGrace <nathan.cole.grace@gmail.com>
2025-06-15 14:07:00 -0500

    testing email

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/spf13/cast v1.9.2 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/image v0.28.0 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"log"
-	"os"
+	"encoding/json"
+	"net/http"
+	"net/mail"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/mailer"
 )
 
 func main() {
 	app := pocketbase.New()
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-		// serves static files from the provided public dir (if exists)
-		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+		se.Router.POST("/api/send-email", func(e *core.RequestEvent) error {
+			data := struct {
+				To      string `json:"to"`
+				Subject string `json:"subject"`
+				Body    string `json:"body"`
+			}{}
+
+			if err := json.NewDecoder(e.Request.Body).Decode(&data); err != nil {
+				return apis.NewBadRequestError("Invalid data", err)
+			}
+
+			message := &mailer.Message{
+				From:    mail.Address{Address: app.Settings().Meta.SenderAddress},
+				To:      []mail.Address{{Address: data.To}},
+				Subject: data.Subject,
+				HTML:    data.Body,
+			}
+
+			if err := app.NewMailClient().Send(message); err != nil {
+				return apis.NewApiError(500, "Send failed", err)
+			}
+
+			return e.JSON(http.StatusOK, map[string]string{"status": "sent"})
+		})
 
 		return se.Next()
 	})
 
 	if err := app.Start(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
