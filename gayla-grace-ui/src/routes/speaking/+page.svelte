065312dfd74080f0973f03cf983e50d8223aaac6
NathanGrace <nathan.cole.grace@gmail.com>
2025-07-13 10:30:17 -0500

    testing bug

--- gayla-grace-ui/src/routes/speaking/+page.svelte
+++ gayla-grace-ui/src/routes/speaking/+page.svelte
@@ -1,20 +1,20 @@
 <script>
 </script>
 
-<div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-    <img src="/assets/speaking/speaking-image-2-blank.png" alt="Gayla Grace Speaking" class="absolute inset-0 -z-10 sm:object-cover min-h-64 min-w-110 w-full" />
-    <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
-        <div class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>
-    <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
-        <div class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-    </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0 lg:pb-30">
-            <h2 class="text-6xl font-bold text-white font-cursive sm:text-7xl lg:text-9xl text-shadow-[0_0_10px_black,_0_0_20px_black,_0_0_40px_black]">Gayla's Speaking</h2>
-        </div>
-    </div>
-</div>
+<!--<div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">-->
+<!--    <img src="/assets/speaking/speaking-image-2-blank.png" alt="Gayla Grace Speaking" class="absolute inset-0 -z-10 sm:object-cover min-h-64 min-w-110 w-full" />-->
+<!--    <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">-->
+<!--        <div class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>-->
+<!--    </div>-->
+<!--    <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">-->
+<!--        <div class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>-->
+<!--    </div>-->
+<!--    <div class="mx-auto max-w-7xl px-6 lg:px-8">-->
+<!--        <div class="mx-auto max-w-2xl lg:mx-0 lg:pb-30">-->
+<!--            <h2 class="text-6xl font-bold text-white font-cursive sm:text-7xl lg:text-9xl text-shadow-[0_0_10px_black,_0_0_20px_black,_0_0_40px_black]">Gayla's Speaking</h2>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 <section class="flex flex-col items-center justify-center mx-8 text-lg">
     <div class="flex flex-col justify-center max-w-220 space-y-4 mt-8">
