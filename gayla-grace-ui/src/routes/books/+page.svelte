c9486980b96245d3161c5d8503d981c7e58d8d99
NathanGrace <nathan.cole.grace@gmail.com>
2025-07-13 20:53:27 -0500

    overflow

--- gayla-grace-ui/src/routes/books/+page.svelte
+++ gayla-grace-ui/src/routes/books/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 
-<div class="py-24 sm:py-32" id="beautifully-blended">
+<div class="py-24 sm:py-32 overflow-hidden" id="beautifully-blended">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
         <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div class="lg:pr-4 flex justify-center">
